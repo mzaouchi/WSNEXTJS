904fbfdb275bab5c3f9f80f32357f6ef1c190acb
mzaouchi <mzaouchi@gmail.com>
2024-08-26 17:55:11 +0100

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ul>
+          <Link href="/"><li>Home</li></Link>
+          <Link href="/Users"><li>Users</li></Link>
+          <Link href="/Contact"><li>Contact</li></Link>
+        </ul>
+        {children}
+      </body>
     </html>
   );
 }
